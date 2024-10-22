1b6de87e64ec85f7dce06e1159869bb7e7b3102c
salloumGeorge <salloumgeorge94@gmail.com>
2024-10-22 21:54:17 +0200

    * Remove custom JAR in favor of spring plugin JAR generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     kotlin("jvm") version "1.9.24"
     application
-    id("com.github.johnrengelman.shadow") version "8.1.0"
+   // id("com.github.johnrengelman.shadow") version "8.1.0"
+    id("org.springframework.boot") version "3.3.4"
+    id("org.jetbrains.kotlin.plugin.spring") version "2.0.21"
 }
 
 group = "org.george"
@@ -12,6 +14,8 @@ repositories {
 }
 
 dependencies {
+    implementation ("org.springframework.boot:spring-boot-starter-web:3.3.4")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
@@ -24,13 +28,14 @@ kotlin {
 }
 
 application {
-    mainClass.set("MainKt") // Update this if your main class is in a package, e.g., "org.george.MainKt"
+    mainClass.set("org.george.KotlinSpringApplicationKt") // Update this if your main class is in a package, e.g., "org.george.MainKt"
 }
 
+/*
 tasks.jar {
     manifest {
         attributes(
             "Main-Class" to application.mainClass.get() // Set the main class in the manifest
         )
     }
-}
+}*/
